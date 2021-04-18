75062ed3ae39f93eeaafcab5dff0530b55b4b302
Vinicius <vinicius.barbosa@rehagro.com.br>
2021-04-18 18:09:51 -0300

    gerando os testes unitarios da classe person

---
+++ Tests/Commands/PersonCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Commands.PersonCommand;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Commands
+{
+    [TestClass]
+    public class PersonCommandTests
+    {
+        [TestMethod]
+        public void ReturnErrorWhenPersonNameIsInvalid()
+        {
+            var personCommand = new PersonCommand();
+            personCommand.Name = "";
+            personCommand.LastName = "Oliveira";
+            personCommand.DocumentType = Domain.enums.DocumentType.CPF;
+            personCommand.DocumentNumber = "11111111111";
+            personCommand.Validate();
+            Assert.AreEqual(false, personCommand.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnErrorWhenPersonNameIsValid()
+        {
+            var personCommand = new PersonCommand();
+            personCommand.Name = "Juarez";
+            personCommand.LastName = "Oliveira";
+            personCommand.DocumentType = Domain.enums.DocumentType.CPF;
+            personCommand.DocumentNumber = "11111111111";
+            personCommand.Validate();
+            Assert.AreEqual(true, personCommand.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnErrorWhenDocumentIsValid()
+        {
+            var personCommand = new PersonCommand();
+            personCommand.Name = "Juarez";
+            personCommand.LastName = "Oliveira";
+            personCommand.DocumentType = Domain.enums.DocumentType.CPF;
+            personCommand.DocumentNumber = "11111111111";
+            personCommand.Validate();
+            Assert.AreEqual(true, personCommand.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnErrorWhenDocumentIsInvalid()
+        {
+            var personCommand = new PersonCommand();
+            personCommand.Name = "Juarez";
+            personCommand.LastName = "Oliveira";
+            personCommand.DocumentType = Domain.enums.DocumentType.CNPJ;
+            personCommand.DocumentNumber = "";
+            personCommand.Validate();
+            Assert.AreEqual(false, personCommand.IsValid);
+        }
+
+    }
+}
--- Tests/Entidades/PersonTests.cs
+++ Tests/Entidades/PersonTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests.Entidades
 {
@@ -6,8 +7,31 @@ namespace Tests.Entidades
     public class PersonTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReturnErrorWhenCNPJIsInvalid()
         {
+            var person = new Person("Antonio", "Augusto", "antonioAugusto@email.com", Domain.enums.DocumentType.CNPJ, "123");
+            Assert.IsTrue(!person.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnSucessWhenCNPJIsValid()
+        {
+            var person = new Person("Antonio", "Augusto", "antonioAugusto@email.com", Domain.enums.DocumentType.CNPJ, "22770620000142");
+            Assert.IsTrue(person.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnErrorWhenCPFIsInvalid()
+        {
+            var person = new Person("Antonio", "Augusto", "antonioAugusto@email.com", Domain.enums.DocumentType.CPF, "123");
+            Assert.IsTrue(!person.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnSucessWhenCPFIsValid()
+        {
+            var person = new Person("Antonio", "Augusto", "antonioAugusto@email.com", Domain.enums.DocumentType.CPF, "55925228006");
+            Assert.IsTrue(person.IsValid);
         }
     }
 }
